f800dac44708160b7e64907957979c4abd676b4f
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-02-23 09:49:01 +0400

    ik
    i

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -15,15 +15,9 @@ const GameOver = () => {
       <h1 style={{fontSize: '2.5vw', marginTop: '0', fontWeight: '300'}}>lnsert coin to continue</h1>
       <h1 style={{fontSize: '2.5vw', marginTop: '-15px', fontWeight: '300', marginBottom: '100px'}}>Oh wait, wrong era!</h1>
 
-        <div style={{display: 'flex', width: '100vw', justifyContent: 'center', alignItems: 'center', gap: '40px'}}>
-            <Link to="/" style={{textDecoration: 'none'}}>
-                <button  style={{backgroundColor: 'transparent', border: 'none', height: '90px', width: '370px', backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `url('${ButtonBox}')`, display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px', cursor: 'grab', fontSize: '30px', color: 'white'}} >MAIN MENU</button>
-            </Link>
-
-            <Link to="/Dashboard" style={{textDecoration: 'none'}}>
-                <button  style={{backgroundColor: 'transparent', border: 'none', height: '90px', width: '370px', backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `url('${ButtonBox}')`, display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px', cursor: 'grab', fontSize: '30px', color: 'white'}} >RETURN</button>
-            </Link>
-        </div>
+      <Link to="/Dashboard" style={{textDecoration: 'none'}}>
+          <button  style={{backgroundColor: 'transparent', border: 'none', height: '90px', width: '370px', backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: `url('${ButtonBox}')`, display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px', cursor: 'grab', fontSize: '30px', color: 'white'}} >RETURN</button>
+      </Link>
 
     </div>
   );
