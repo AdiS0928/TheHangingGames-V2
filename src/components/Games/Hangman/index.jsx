69b24dd6b5228233caac73aa26160076c8d78385
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2024-02-23 14:58:20 +0400

    o
    o

--- src/components/Games/Hangman/index.jsx
+++ src/components/Games/Hangman/index.jsx
@@ -229,7 +229,7 @@ const HangmanGame = () => {
 	// eslint-disable-next-line 
   }, []); // Run only once when the component mounts
   return (
-    <div className='mainhangman'>
+    <div className='mainhangman' style={{marginTop: '-60px'}}>
       <div class="hangman">
         <div class="hangman-results">
           <div class="hangman-hangman">
